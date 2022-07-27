011b54a44013dcbe1e0717d2afe7aeef92de6984
isengrims <104489572+isengrims@users.noreply.github.com>
2022-07-27 20:42:34 +0300

    [TH2-3191] Handle body raw message in StoredMessage.content
    * [TH2-3191] StoredMessage.content() now contains raw message body.

--- src/main/kotlin/com/exactpro/th2/lwdataprovider/grpc/GrpcUtils.kt
+++ src/main/kotlin/com/exactpro/th2/lwdataprovider/grpc/GrpcUtils.kt
@@ -16,14 +16,19 @@
 
 package com.exactpro.th2.lwdataprovider.grpc
 
+import com.exactpro.cradle.messages.StoredMessage
 import com.exactpro.cradle.messages.StoredMessageId
 import com.exactpro.th2.common.grpc.ConnectionID
 import com.exactpro.th2.common.grpc.Direction
 import com.exactpro.th2.common.grpc.MessageID
+import com.exactpro.th2.common.grpc.RawMessage
+import com.exactpro.th2.common.message.toTimestamp
 import com.exactpro.th2.dataprovider.grpc.MessageStream
 import com.exactpro.th2.dataprovider.grpc.TimeRelation
 import com.exactpro.th2.lwdataprovider.entities.requests.ProviderMessageStream
+import com.google.protobuf.ByteString
 import com.google.protobuf.Timestamp
+import com.google.protobuf.UnsafeByteOperations
 import java.time.Instant
 
 fun Timestamp.toInstant() : Instant = Instant.ofEpochSecond(this.seconds, this.nanos.toLong())
@@ -63,4 +68,17 @@ fun StoredMessageId.toGrpcMessageId(): MessageID {
 
 fun MessageStream.toProviderMessageStreams(): ProviderMessageStream {
     return ProviderMessageStream(this.name, this.direction.toCradleDirection())
+}
+
+fun StoredMessage.toRawMessage(): RawMessage {
+    val message = this
+    return RawMessage.newBuilder().apply {
+        metadataBuilder.apply {
+            putAllProperties(message.metadata?.toMap() ?: emptyMap())
+            id = message.id.toGrpcMessageId()
+            timestamp = message.timestamp.toTimestamp()
+            protocol = message.protocol ?: ""
+        }.build()
+        body = UnsafeByteOperations.unsafeWrap(message.content)
+    }.build()
 }

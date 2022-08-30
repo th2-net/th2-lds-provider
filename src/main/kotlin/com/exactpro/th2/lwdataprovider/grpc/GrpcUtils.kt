ef66ecc5cbd968ac983ebc77447ca622c6601d1e
Dmitriy-Yugay <mr.dmitriy.yugay@gmail.com>
2022-08-30 11:58:04 +0400

    added logging to format converter

--- src/main/kotlin/com/exactpro/th2/lwdataprovider/grpc/GrpcUtils.kt
+++ src/main/kotlin/com/exactpro/th2/lwdataprovider/grpc/GrpcUtils.kt
@@ -28,7 +28,10 @@ import com.exactpro.th2.dataprovider.grpc.MessageSearchRequest.ResponseFormat.AL
 import com.exactpro.th2.dataprovider.grpc.MessageSearchRequest.ResponseFormat.BASE_64
 import com.exactpro.th2.dataprovider.grpc.MessageSearchRequest.ResponseFormat.PARSED
 import com.google.protobuf.Timestamp
+import mu.KotlinLogging
 import java.time.Instant
+
+private val logger = KotlinLogging.logger { }
 
 fun List<com.exactpro.th2.dataprovider.grpc.MessageSearchRequest.ResponseFormat>?.toLocalResponseFormats() : List<ResponseFormat> {
     val list : MutableList<ResponseFormat> = ArrayList()
@@ -40,7 +43,9 @@ fun List<com.exactpro.th2.dataprovider.grpc.MessageSearchRequest.ResponseFormat>
                 ALL -> list.add(ResponseFormat.ALL)
                 BASE_64 -> list.add(ResponseFormat.BASE_64)
                 PARSED -> list.add(ResponseFormat.PARSED)
-                else -> {}
+                else -> {
+                    logger.error { "Unrecognized response format: $responseFormat" }
+                }
             }
         }
     return  list.toList()

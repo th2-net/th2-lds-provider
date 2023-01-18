th2-net/th2-lw-data-provider

[TH2-3597] Add http endpoint for requesting groups

src/main/kotlin/com/exactpro/th2/lwdataprovider/http/HttpServer.kt
@@ -56,6 +56,11 @@ class HttpServer(private val context: Context) {
         ), "/search/sse/messages")
 
         servletHandler.addServletWithMapping(ServletHolder(
+            GetMessageGroupsServlet(configuration, jacksonMapper, keepAliveHandler,
+                searchMessagesHandler)
+        ), "/search/sse/messages/group")
+
+        servletHandler.addServletWithMapping(ServletHolder(
             GetMessageById(configuration, jacksonMapper, keepAliveHandler,
                 searchMessagesHandler)
         ), "/message/*")

th2-net/th2-lds-provider

src/main/kotlin/com/exactpro/th2/lwdataprovider/http/HttpServer.kt
@@ -61,6 +61,11 @@ class HttpServer(private val context: Context) {
         ), "/search/sse/messages")
 
         servletHandler.addServletWithMapping(ServletHolder(
+            GetMessageGroupsServlet(configuration, sseResponseBuilder, keepAliveHandler,
+                searchMessagesHandler, context.dataMeasurement)
+        ), "/search/sse/messages/group")
+
+        servletHandler.addServletWithMapping(ServletHolder(
             GetMessageById(
                 sseResponseBuilder, keepAliveHandler, searchMessagesHandler,
                 context.dataMeasurement
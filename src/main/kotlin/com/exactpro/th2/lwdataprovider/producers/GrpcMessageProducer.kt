51974cc868fc5d920fb786d83d1a1372929821a0
isengrims <104489572+isengrims@users.noreply.github.com>
2023-06-06 01:20:08 +0400

    Added message properties into grpc search response (#50)

--- src/main/kotlin/com/exactpro/th2/lwdataprovider/producers/GrpcMessageProducer.kt
+++ src/main/kotlin/com/exactpro/th2/lwdataprovider/producers/GrpcMessageProducer.kt
@@ -37,6 +37,7 @@ class GrpcMessageProducer {
             return MessageGroupResponse.newBuilder().apply {
                 messageId = convertMessageId(storedMessage.id)
                 timestamp = convertTimestamp(storedMessage.timestamp)
+                storedMessage.metadata?.toMap()?.let(this@apply::putAllMessageProperties)
 
                 if (responseFormats.isEmpty() || ResponseFormat.BASE_64 in responseFormats) {
                     bodyRaw = rawMessage.rawMessage.body

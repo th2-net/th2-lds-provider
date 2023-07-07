81ad2e0f8af3a5865bc597b97308c4f514ae3382
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2023-07-07 14:09:00 +0400

    [TH2-4887] Removed setupSwagger from code

--- src/main/kotlin/com/exactpro/th2/lwdataprovider/http/HttpServer.kt
+++ src/main/kotlin/com/exactpro/th2/lwdataprovider/http/HttpServer.kt
@@ -39,8 +39,8 @@ import io.javalin.openapi.plugin.OpenApiPlugin
 import io.javalin.openapi.plugin.OpenApiPluginConfiguration
 import io.javalin.openapi.plugin.redoc.ReDocConfiguration
 import io.javalin.openapi.plugin.redoc.ReDocPlugin
-import io.javalin.openapi.plugin.swagger.SwaggerConfiguration
-import io.javalin.openapi.plugin.swagger.SwaggerPlugin
+//import io.javalin.openapi.plugin.swagger.SwaggerConfiguration
+//import io.javalin.openapi.plugin.swagger.SwaggerPlugin
 import io.javalin.validation.JavalinValidation
 import io.micrometer.core.instrument.Clock
 import io.micrometer.core.instrument.Tag
@@ -146,7 +146,7 @@ class HttpServer(private val context: Context) {
 
             setupOpenApi(it)
 
-            setupSwagger(it)
+//            setupSwagger(it)
 
             setupReDoc(it)
         }.apply {
@@ -172,10 +172,10 @@ class HttpServer(private val context: Context) {
         it.plugins.register(ReDocPlugin(reDocConfiguration))
     }
 
-    private fun setupSwagger(it: JavalinConfig) {
-        val swaggerConfiguration = SwaggerConfiguration()
-        it.plugins.register(SwaggerPlugin(swaggerConfiguration))
-    }
+//    private fun setupSwagger(it: JavalinConfig) {
+//        val swaggerConfiguration = SwaggerConfiguration()
+//        it.plugins.register(SwaggerPlugin(swaggerConfiguration))
+//    }
 
     private fun setupOpenApi(it: JavalinConfig) {
 
